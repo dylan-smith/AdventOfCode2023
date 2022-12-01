744501f46a7b95de89931795f856f877e384c85f
Dylan Smith <dysmith@microsoft.com>
2022-12-01 00:10:29 -0500

    day 1 solved

--- src/Days/Day01.cs
+++ src/Days/Day01.cs
@@ -5,11 +5,57 @@ public class Day01 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var calories = input.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.None);
+
+        var elfs = new List<int>();
+        var elf = 0;
+
+        foreach (var c in calories)
+        {
+            if (c != string.Empty)
+            {
+                elf += int.Parse(c);
+            }
+            
+            if (c == string.Empty)
+            {
+                elfs.Add(elf);
+                elf = 0;
+            }
+        }
+
+        elfs.Add(elf);
+
+        return elfs.Max().ToString();
     }
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        var calories = input.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.None);
+
+        var elfs = new List<int>();
+        var elf = 0;
+
+        foreach (var c in calories)
+        {
+            if (c != string.Empty)
+            {
+                elf += int.Parse(c);
+            }
+
+            if (c == string.Empty)
+            {
+                elfs.Add(elf);
+                elf = 0;
+            }
+        }
+
+        elfs.Add(elf);
+
+        var count = elfs.Count;
+
+        elfs.Sort();
+
+        return (elfs[count - 1] + elfs[count - 2] + elfs[count - 3]).ToString();
     }
 }
