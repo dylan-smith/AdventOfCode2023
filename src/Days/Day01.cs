92a741ee111288902fde8e51b9534115eba2caf7
Dylan Smith <dysmith@microsoft.com>
2022-12-01 00:16:30 -0500

    refactor

--- src/Days/Day01.cs
+++ src/Days/Day01.cs
@@ -5,56 +5,20 @@ public class Day01 : BaseDay
 {
     public override string PartOne(string input)
     {
-        var calories = input.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.None);
+        var elfs = input.Paragraphs();
+        var calories = elfs.Select(e => e.Integers().Sum());
 
-        var elfs = new List<int>();
-        var elf = 0;
-
-        foreach (var c in calories)
-        {
-            if (c != string.Empty)
-            {
-                elf += int.Parse(c);
-            }
-            
-            if (c == string.Empty)
-            {
-                elfs.Add(elf);
-                elf = 0;
-            }
-        }
-
-        elfs.Add(elf);
-
-        return elfs.Max().ToString();
+        return calories.Max().ToString();
     }
 
     public override string PartTwo(string input)
     {
-        var calories = input.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.None);
+        var elfs = input.Paragraphs();
+        var calories = elfs.Select(e => e.Integers().Sum()).ToList();
 
-        var elfs = new List<int>();
-        var elf = 0;
+        calories.Sort();
 
-        foreach (var c in calories)
-        {
-            if (c != string.Empty)
-            {
-                elf += int.Parse(c);
-            }
-
-            if (c == string.Empty)
-            {
-                elfs.Add(elf);
-                elf = 0;
-            }
-        }
-
-        elfs.Add(elf);
-
-        elfs.Sort();
-
-        var result = elfs.Window(3).Last().Sum();
+        var result = calories.Window(3).Last().Sum();
 
         return result.ToString();
     }
