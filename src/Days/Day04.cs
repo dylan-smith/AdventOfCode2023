48ad781ab4db65cc0476e293850a7893f0ced291
Dylan Smith <dysmith@microsoft.com>
2022-12-04 07:49:27 -0500

    Day 4

--- src/Days/Day04.cs
+++ src/Days/Day04.cs
@@ -5,11 +5,52 @@ public class Day04 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var lines = input.Lines().ToList();
+        var score = 0;
+        
+        foreach (var line in lines)
+        {
+            var words = line.Words().ToList();
+            var firstElf = words.First().Split('-');
+            var secondElf = words.Last().Split('-');
+
+            if (int.Parse(firstElf[0]) <= int.Parse(secondElf[0]) && int.Parse(firstElf[1]) >= int.Parse(secondElf[1]))
+            {
+                score++;
+            }
+            else
+            {
+                if (int.Parse(secondElf[0]) <= int.Parse(firstElf[0]) && int.Parse(secondElf[1]) >= int.Parse(firstElf[1]))
+                {
+                    score++;
+                }
+            }    
+        }
+
+        return score.ToString();
     }
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        var lines = input.Lines().ToList();
+        var score = 0;
+
+        foreach (var line in lines)
+        {
+            var words = line.Words().ToList();
+            var firstElf = words.First().Split('-');
+            var secondElf = words.Last().Split('-');
+
+            var firstElfNums = firstElf.Select(int.Parse);
+            var secondElfNums = secondElf.Select(int.Parse);
+
+            if ((firstElfNums.Last() >= secondElfNums.First() && firstElfNums.First() <= secondElfNums.Last()) || 
+                (secondElfNums.Last() >= firstElfNums.First() && secondElfNums.First() <= firstElfNums.First()))
+            {
+                score++;
+            }
+        }
+
+        return score.ToString();
     }
 }
