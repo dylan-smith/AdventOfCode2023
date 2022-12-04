49d113c5cd0180593de25e906131edbc15ad1c62
Dylan Smith <dysmith@microsoft.com>
2022-12-04 08:05:31 -0500

    refactor day 4

--- src/Days/Day04.cs
+++ src/Days/Day04.cs
@@ -1,56 +1,25 @@
-﻿namespace AdventOfCode.Days;
+﻿using AdventOfCode.Utility;
+
+namespace AdventOfCode.Days;
 
 [Day(2022, 4)]
 public class Day04 : BaseDay
 {
     public override string PartOne(string input)
     {
-        var lines = input.Lines().ToList();
-        var score = 0;
-        
-        foreach (var line in lines)
-        {
-            var words = line.Words().ToList();
-            var firstElf = words.First().Split('-');
-            var secondElf = words.Last().Split('-');
+        var elfPairs = input.ParseLines(l => l.Words().Select(w => new NumberRange(w)));
 
-            if (int.Parse(firstElf[0]) <= int.Parse(secondElf[0]) && int.Parse(firstElf[1]) >= int.Parse(secondElf[1]))
-            {
-                score++;
-            }
-            else
-            {
-                if (int.Parse(secondElf[0]) <= int.Parse(firstElf[0]) && int.Parse(secondElf[1]) >= int.Parse(firstElf[1]))
-                {
-                    score++;
-                }
-            }    
-        }
+        var score = elfPairs.Count(elf => elf.First().Contains(elf.Last()) || elf.Last().Contains(elf.First()));
 
         return score.ToString();
     }
 
     public override string PartTwo(string input)
     {
-        var lines = input.Lines().ToList();
-        var score = 0;
+        var elfPairs = input.ParseLines(l => l.Words().Select(w => new NumberRange(w)));
 
-        foreach (var line in lines)
-        {
-            var words = line.Words().ToList();
-            var firstElf = words.First().Split('-');
-            var secondElf = words.Last().Split('-');
-
-            var firstElfNums = firstElf.Select(int.Parse);
-            var secondElfNums = secondElf.Select(int.Parse);
-
-            if ((firstElfNums.Last() >= secondElfNums.First() && firstElfNums.First() <= secondElfNums.Last()) || 
-                (secondElfNums.Last() >= firstElfNums.First() && secondElfNums.First() <= firstElfNums.First()))
-            {
-                score++;
-            }
-        }
-
+        var score = elfPairs.Count(elf => elf.First().Intersects(elf.Last()));
+        
         return score.ToString();
     }
 }
---
+++ src/Utility/NumberRange.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Utility
+{
+    public class NumberRange
+    {
+        public int Start { get; }
+        public int End { get; }
+
+        public NumberRange(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public NumberRange(string range)
+        {
+            var split = range.Split('-');
+            Start = int.Parse(split[0]);
+            End = int.Parse(split[1]);
+        }
+
+        public bool Contains(int number) => number >= Start && number <= End;
+
+        public bool Contains(NumberRange range)
+        {
+            return range.Start >= Start && range.End <= End;
+        }
+
+        public bool Intersects(NumberRange range)
+        {
+            return range.Start <= End && range.End >= Start;
+        }
+    }
+}
