1032bbebd66a4bc94342b9abf7357f05ead80fc1
Dylan Smith <dysmith@microsoft.com>
2022-12-06 14:48:40 -0500

    Day 6

--- src/Days/Day06.cs
+++ src/Days/Day06.cs
@@ -5,11 +5,35 @@ public class Day06 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var count = 3;
+        
+        foreach (var window in input.Window(4))
+        {
+            count++;
+
+            if (window.Distinct().Count() == 4)
+            {
+                return count.ToString();
+            }
+        }
+
+        throw new Exception();
     }
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        var count = 13;
+
+        foreach (var window in input.Window(14))
+        {
+            count++;
+
+            if (window.Distinct().Count() == 14)
+            {
+                return count.ToString();
+            }
+        }
+
+        throw new Exception();
     }
 }
