01327d756ea9d2cfbe741ef503b8aee1e34437cb
Dylan Smith <dysmith@microsoft.com>
2022-12-07 13:46:31 -0500

    refactor day 7.1

--- src/Days/Day07.cs
+++ src/Days/Day07.cs
@@ -1,71 +1,31 @@
 ﻿namespace AdventOfCode.Days;
 
+public enum CommandType
+{
+    cd,
+    ls
+}
+
 [Day(2022, 7)]
 public class Day07 : BaseDay
 {
     public override string PartOne(string input)
     {
-        var commands = input.Split('$', StringSplitOptions.RemoveEmptyEntries);
+        var commands = ParseCommands(input);
+        
         var fileSystem = new Tree<(bool isFile, int size, string name)>((false, 0, "/"));
         var pwd = fileSystem;
 
         foreach (var command in commands.Skip(1))
         {
-            var lines = command.Lines().ToList();
-            var cmd = lines.First().Trim();
-            var output = lines.Skip(1);
-            
-            if (cmd.StartsWith("cd"))
+            if (command.type == CommandType.cd)
             {
-                var dir = cmd.Words().Last();
-
-                if (dir == "..")
-                {
-                    pwd = pwd.Parent;
-                }
-                else
-                {
-                    if (pwd.Children.Any(x => x.Data.name == dir))
-                    {
-                        pwd = pwd.Children.First(x => x.Data.name == dir);
-                    }
-                    else
-                    {
-                        var newDir = new Tree<(bool isFile, int size, string name)>((false, 0, dir));
-                        newDir.Parent = pwd;
-                        pwd = pwd.Children.AddLast(newDir).Value;
-                    }
-                }
+                pwd = ExecuteCD(command, pwd);
+                
             }
-            else if (cmd.StartsWith("ls"))
+            else if (command.type == CommandType.ls)
             {
-                foreach (var item in output)
-                {
-                    if (item.StartsWith("dir"))
-                    {
-                        var dir = item.Words().Last();
-
-                        if (!pwd.Children.Any(x => x.Data.name == dir))
-                        {
-                            var newDir = new Tree<(bool isFile, int size, string name)>((false, 0, dir));
-                            newDir.Parent = pwd;
-                            _ = pwd.Children.AddLast(newDir);
-                        }
-                    }
-                    else
-                    {
-                        var words = item.Words();
-                        var size = int.Parse(words.First());
-                        var name = words.Last();
-
-                        if (!pwd.Children.Any(x => x.Data.name == name))
-                        {
-                            var newFile = new Tree<(bool isFile, int size, string name)>((true, size, name));
-                            newFile.Parent = pwd;
-                            _ = pwd.Children.AddLast(newFile);
-                        }
-                    }
-                }
+                ExecuteLS(command, pwd);
             }
         }
 
@@ -73,6 +33,81 @@ public class Day07 : BaseDay
         var dirSizes = GetDirSizes(fileSystem);
 
         return dirSizes.Where(x => x.size <= 100000).Sum(x => x.size).ToString();
+    }
+
+    private void ExecuteLS((CommandType type, string command, IEnumerable<string> output) command, Tree<(bool isFile, int size, string name)> pwd)
+    {
+        foreach (var item in command.output)
+        {
+            if (item.StartsWith("dir"))
+            {
+                var dir = item.Words().Last();
+
+                if (!pwd.Children.Any(x => x.Data.name == dir))
+                {
+                    var newDir = new Tree<(bool isFile, int size, string name)>((false, 0, dir));
+                    newDir.Parent = pwd;
+                    _ = pwd.Children.AddLast(newDir);
+                }
+            }
+            else
+            {
+                var words = item.Words();
+                var size = int.Parse(words.First());
+                var name = words.Last();
+
+                if (!pwd.Children.Any(x => x.Data.name == name))
+                {
+                    var newFile = new Tree<(bool isFile, int size, string name)>((true, size, name));
+                    newFile.Parent = pwd;
+                    _ = pwd.Children.AddLast(newFile);
+                }
+            }
+        }
+    }
+
+    private Tree<(bool isFile, int size, string name)> ExecuteCD((CommandType type, string command, IEnumerable<string> output) command, Tree<(bool isFile, int size, string name)> pwd)
+    {
+        var dir = command.command.Words().Last();
+
+        if (dir == "..")
+        {
+            return pwd.Parent;
+        }
+        else
+        {
+            if (pwd.Children.Any(x => x.Data.name == dir))
+            {
+                return pwd.Children.First(x => x.Data.name == dir);
+            }
+            else
+            {
+                var newDir = new Tree<(bool isFile, int size, string name)>((false, 0, dir));
+                newDir.Parent = pwd;
+                return pwd.Children.AddLast(newDir).Value;
+            }
+        }
+    }
+
+    private IEnumerable<(CommandType type, string command, IEnumerable<string> output)> ParseCommands(string input)
+    {
+        var commands = input.Split('$', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var command in commands)
+        {
+            var lines = command.Lines().ToList();
+            var cmd = lines.First().Trim();
+            var output = lines.Skip(1);
+
+            if (cmd.StartsWith("cd"))
+            {
+                yield return (CommandType.cd, cmd, output);
+            }
+            else if (cmd.StartsWith("ls"))
+            {
+                yield return (CommandType.ls, cmd, output);
+            }
+        }
     }
 
     private void CalculateDirSizes(Tree<(bool isFile, int size, string name)> fileSystem)
