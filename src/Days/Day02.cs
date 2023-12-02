8f455048d2e4b11d376f50b7307a144ec354bb03
Dylan Smith <dylanfromwinnipeg@gmail.com>
2023-12-02 04:44:36 -0700

    Extracted Game class

--- src/Days/Day02.cs
+++ src/Days/Day02.cs
@@ -1,67 +1,30 @@
-﻿namespace AdventOfCode.Days;
+﻿using System.Windows.Shapes;
+
+namespace AdventOfCode.Days;
 
 [Day(2023, 2)]
 public class Day02 : BaseDay
 {
     public override string PartOne(string input)
     {
-        var games = input.Lines().Select(line => ParseGame(line));
+        var games = input.Lines().Select(line => new Game(line));
 
         var bag = new Dictionary<string, long>();
         bag.Add("red", 12);
         bag.Add("green", 13);
         bag.Add("blue", 14);
 
-        var possibleGames = games.Where(game => IsGamePossible(game, bag));
+        var possibleGames = games.Where(game => game.IsGamePossible(bag));
         var result = possibleGames.Sum(g => g.Id);
 
         return result.ToString();
     }
 
-    private (long Id, List<Dictionary<string, long>> Handfuls) ParseGame(string line)
-    {
-        var id = line.Split(':')[0].Words().Last();
-        var handfuls = new List<Dictionary<string, long>>();
-
-        var handfulTexts = line.Split(':')[1].Split(';');
-
-        foreach (var h in handfulTexts)
-        {
-            var words = h.Words().ToList();
-            var cubes = new Dictionary<string, long>();
-
-            for (var i = 0; i < words.Count(); i += 2)
-            {
-                cubes.Add(words[i + 1], long.Parse(words[i]));
-            }
-
-            handfuls.Add(cubes);
-        }
-
-        return (long.Parse(id), handfuls);
-    }
-
-    private bool IsGamePossible((long Id, List<Dictionary<string, long>> Handfuls) game, Dictionary<string, long> bag)
-    {
-        foreach (var handful in game.Handfuls)
-        {
-            foreach (var cube in handful)
-            {
-                if (cube.Value > bag[cube.Key])
-                {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
     public override string PartTwo(string input)
     {
-        var games = input.Lines().Select(line => ParseGame(line));
+        var games = input.Lines().Select(line => new Game(line));
 
-        var minCubes = games.Select(game => FindMinimumCubes(game));
+        var minCubes = games.Select(game => game.FindMinimumCubes());
         var result = minCubes.Sum(x => CalculatePower(x));
 
         return result.ToString();
@@ -72,28 +35,71 @@ public class Day02 : BaseDay
         return cubes.Multiply(x => x.Value);
     }
 
-    private Dictionary<string, long> FindMinimumCubes((long Id, List<Dictionary<string, long>> Handfuls) game)
+    private class Game
     {
-        var result = new Dictionary<string, long>();
+        public long Id { get; set; }
+        public List<Dictionary<string, long>> Handfuls { get; set; }
 
-        foreach (var handful in game.Handfuls)
+        public Game(string input)
         {
-            foreach (var cube in handful)
+            Id = long.Parse(input.Split(':')[0].Words().Last());
+            Handfuls = new List<Dictionary<string, long>>();
+
+            var handfulTexts = input.Split(':')[1].Split(';');
+
+            foreach (var h in handfulTexts)
             {
-                if (result.ContainsKey(cube.Key))
+                var words = h.Words().ToList();
+                var cubes = new Dictionary<string, long>();
+
+                for (var i = 0; i < words.Count; i += 2)
                 {
-                    if (result[cube.Key] < cube.Value)
-                    {
-                        result[cube.Key] = cube.Value;
-                    }
+                    cubes.Add(words[i + 1], long.Parse(words[i]));
                 }
-                else
-                {
-                    result.Add(cube.Key, cube.Value);
-                }
+
+                Handfuls.Add(cubes);
             }
         }
 
-        return result;
+        public bool IsGamePossible(Dictionary<string, long> bag)
+        {
+            foreach (var handful in Handfuls)
+            {
+                foreach (var cube in handful)
+                {
+                    if (cube.Value > bag[cube.Key])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public Dictionary<string, long> FindMinimumCubes()
+        {
+            var result = new Dictionary<string, long>();
+
+            foreach (var handful in Handfuls)
+            {
+                foreach (var cube in handful)
+                {
+                    if (result.ContainsKey(cube.Key))
+                    {
+                        if (result[cube.Key] < cube.Value)
+                        {
+                            result[cube.Key] = cube.Value;
+                        }
+                    }
+                    else
+                    {
+                        result.Add(cube.Key, cube.Value);
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
