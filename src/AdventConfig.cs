40b0077a207b369b135bfac371af00118a138134
Dylan Smith <dylanfromwinnipeg@gmail.com>
2023-12-05 15:42:43 -0500

    config update

--- src/AdventConfig.cs
+++ src/AdventConfig.cs
@@ -3,7 +3,7 @@
     public static class AdventConfig
     {
         public const int DefaultYear = 2023;
-        public const int DefaultDay = 4;
+        public const int DefaultDay = 5;
         public const string LogFileFolder = @"C:\git\AdventOfCode2023\logs";
         public const string InputFileFolder = @"C:\git\AdventOfCode2023\input";
         public const string TestFileFolder = @"C:\git\AdventOfCode2023\tests";
