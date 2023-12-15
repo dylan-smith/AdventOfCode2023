9c2d2a7ffb93d0782e2b31676c0f6c936d3a996a
Dylan Smith <dylanfromwinnipeg@gmail.com>
2023-12-15 12:35:35 -0500

    day 5.2

--- src/Days/Day05.cs
+++ src/Days/Day05.cs
@@ -46,10 +46,75 @@ public class Day05 : BaseDay
         return input.Split(":")[1].Longs().ToList();
     }
 
+    private List<(long start, long length)> ParseSeeds2(string input)
+    {
+        var numbers = input.Split(":")[1].Longs().ToList();
+
+        var result = new List<(long start, long length)>();
+
+        for (var i = 0; i < numbers.Count; i += 2)
+        {
+            result.Add((numbers[i], numbers[i + 1]));
+        }
+
+        return result;
+    }
+
+    private List<(long start, long length)> _seeds;
+    private List<(long dest, long source, long length)> _seedToSoilMap;
+    private List<(long dest, long source, long length)> _soilToFertilizerMap;
+    private List<(long dest, long source, long length)> _fertilizerToWaterMap;
+    private List<(long dest, long source, long length)> _waterToLightMap;
+    private List<(long dest, long source, long length)> _lightToTemperatureMap;
+    private List<(long dest, long source, long length)> _temperatureToHumidityMap;
+    private List<(long dest, long source, long length)> _humidityToLocationMap;
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        var paragraphs = input.Paragraphs().ToList();
+
+        _seeds = ParseSeeds2(paragraphs[0]);
+        _seedToSoilMap = ParseMap(paragraphs[1]);
+        _soilToFertilizerMap = ParseMap(paragraphs[2]);
+        _fertilizerToWaterMap = ParseMap(paragraphs[3]);
+        _waterToLightMap = ParseMap(paragraphs[4]);
+        _lightToTemperatureMap = ParseMap(paragraphs[5]);
+        _temperatureToHumidityMap = ParseMap(paragraphs[6]);
+        _humidityToLocationMap = ParseMap(paragraphs[7]);
+
+        var location = 0L;
+
+        while (!LocationInSeeds(location))
+        {
+            if (location % 10000000 == 0)
+            {
+                Log(location.ToString());
+            }
+            location++;
+        }
+
+        return location.ToString();
+    }
+
+    private bool LocationInSeeds(long location)
+    {
+        var seed = location.ApplyMapReverse(_humidityToLocationMap)
+                           .ApplyMapReverse(_temperatureToHumidityMap)
+                           .ApplyMapReverse(_lightToTemperatureMap)
+                           .ApplyMapReverse(_waterToLightMap)
+                           .ApplyMapReverse(_fertilizerToWaterMap)
+                           .ApplyMapReverse(_soilToFertilizerMap)
+                           .ApplyMapReverse(_seedToSoilMap);
+
+        foreach (var range in _seeds)
+        {
+            if (seed >= range.start && seed < range.start + range.length)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
@@ -66,5 +131,18 @@ public static class Day05Extensions
         }
 
         return seed;
+    }
+
+    public static long ApplyMapReverse(this long target, List<(long dest, long source, long length)> map)
+    {
+        foreach (var range in map)
+        {
+            if (target >= range.dest && target < range.dest + range.length)
+            {
+                return target + (range.source - range.dest);
+            }
+        }
+
+        return target;
     }
 }
