e70a6eec5982d80e985ae0d23e7565246ead5796
Dylan Smith <dylanfromwinnipeg@gmail.com>
2024-08-08 23:27:21 -0500

    Day 8.1

--- src/Days/Day08.cs
+++ src/Days/Day08.cs
@@ -1,15 +1,79 @@
-﻿namespace AdventOfCode.Days;
+﻿
+namespace AdventOfCode.Days;
 
 [Day(2023, 8)]
 public class Day08 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var direction = new LinkedList<char>(input.Lines().First()).First;
+        var nodes = MakeNodesDictionary(input.Lines().Skip(1));
+
+        var pos = nodes["AAA"];
+        var moves = 0;
+
+        while (pos.Name != "ZZZ")
+        {
+            var newNodeName = UpdatePosition(pos, direction.Value);
+            pos = nodes[newNodeName];
+            direction = direction.NextCircular();
+            moves++;
+        }
+
+        return moves.ToString();
+    }
+
+    private string UpdatePosition(MapNode pos, char value)
+    {
+        return value == 'L' ? pos.Left : pos.Right;
+    }
+
+    private (string Name, string Left, string Right) ParseNode(string input)
+    {
+        var pieces = input.Split(new string[] { " ", "=", "(", ")", "," }, StringSplitOptions.RemoveEmptyEntries);
+
+        return (pieces[0], pieces[1], pieces[2]);
     }
 
     public override string PartTwo(string input)
     {
-        return string.Empty;
+        //var direction = new LinkedList<char>(input.Lines().First()).First;
+        //var nodes = MakeNodesDictionary(input.Lines().Skip(1));
+        ////var nodes = input.Lines().Skip(1).Select(ParseNode).ToList();
+
+        //var curNodes = nodes.Where(n => n.Key.EndsWith('A')).ToList();
+        var moves = 0L;
+
+        //while (curNodes.Any(n => !n.Key.EndsWith('Z')))
+        //{
+        //    var newNodes = new List<KeyValuePair<string , (string Left, string Right)>>();
+
+        //    foreach (var node in curNodes)
+        //    {
+        //        var newNodeName = UpdatePosition(node.Value, direction.Value);
+        //        newNodes.Add(nodes[newNodeName]);
+        //    }
+
+        //    curNodes = newNodes;
+        //    direction = direction.NextCircular();
+        //    moves++;
+        //}
+
+        return moves.ToString();
     }
+
+    private IDictionary<string, MapNode> MakeNodesDictionary(IEnumerable<string> lines)
+    {
+        var result = new Dictionary<string, MapNode>();
+
+        foreach (var line in lines)
+        {
+            var pieces = line.Split(new string[] { " ", "=", "(", ")", "," }, StringSplitOptions.RemoveEmptyEntries);
+            result.Add(pieces[0], new(pieces[0], pieces[1], pieces[2]));
+        }
+
+        return result;
+    }
+
+    private record MapNode(string Name, string Left, string Right);
 }
