4ac3495389973c9db8afed2ea3e3a3afd69413d7
Dylan Smith <dylanfromwinnipeg@gmail.com>
2024-08-10 12:14:54 -0500

    refactoring Day 9

--- src/Days/Day09.cs
+++ src/Days/Day09.cs
@@ -1,92 +1,79 @@
-﻿
-
-namespace AdventOfCode.Days;
+﻿namespace AdventOfCode.Days;
 
 [Day(2023, 9)]
 public class Day09 : BaseDay
 {
     public override string PartOne(string input)
     {
-        var sensors = input.ParseLines(ParseSensor).ToList();
+        var sensors = input.ParseLines(ParseSensor);
         var result = 0L;
 
         foreach (var sensor in sensors)
         {
-            var diffList = CreateDiffList(sensor);
-            AddItemToDiffList(diffList);
-            result += GetNextItem(diffList);
+            var diffLists = CreateDiffLists(sensor);
+            AddItemToEndOfDiffList(diffLists);
+            result += GetNextItem(diffLists);
         }
 
         return result.ToString();
     }
 
-    private long GetNextItem(List<List<long>> diffList)
-    {
-        return diffList[0].Last();
-    }
-
-    private void AddItemToDiffList(List<List<long>> diffList)
-    {
-        diffList.Last().Add(0);
-
-        for (var i = diffList.Count - 2; i >= 0; i--)
-        {
-            diffList[i].Add(diffList[i].Last() + diffList[i + 1].Last());
-        }
-    }
-
-    private List<List<long>> CreateDiffList(IEnumerable<long> sensor)
-    {
-        var result = new List<List<long>>
-        {
-            new(sensor)
-        };
-
-        while (result.Last().Any(x => x != 0))
-        {
-            result.Add(new List<long>());
-
-            for (var i = 1; i < result[^2].Count; i++)
-            {
-                result.Last().Add(result[^2][i] - result[^2][i - 1]);
-            }
-        }
-
-        return result;
-    }
-
-    private List<long> ParseSensor(string line)
-    {
-        return line.Longs().ToList();
-    }
-
     public override string PartTwo(string input)
     {
-        var sensors = input.ParseLines(ParseSensor).ToList();
+        var sensors = input.ParseLines(ParseSensor);
         var result = 0L;
 
         foreach (var sensor in sensors)
         {
-            var diffList = CreateDiffList(sensor);
-            AddPreviousItemToDiffList(diffList);
+            var diffList = CreateDiffLists(sensor);
+            AddItemToStartOfDiffList(diffList);
             result += GetFirstItem(diffList);
         }
 
         return result.ToString();
     }
 
-    private long GetFirstItem(List<List<long>> diffList)
+    private IEnumerable<long> ParseSensor(string line) => line.Longs();
+    private long GetFirstItem(List<List<long>> diffList) => diffList[0].First();
+    private long GetNextItem(List<List<long>> diffList) => diffList[0].Last();
+
+    private List<List<long>> CreateDiffLists(IEnumerable<long> sensor)
     {
-        return diffList[0].First();
+        var diffLists = new List<List<long>>
+        {
+            new(sensor)
+        };
+
+        while (diffLists.Last().Any(x => x != 0))
+        {
+            diffLists.Add(new List<long>());
+
+            for (var i = 1; i < diffLists[^2].Count; i++)
+            {
+                diffLists.Last().Add(diffLists[^2][i] - diffLists[^2][i - 1]);
+            }
+        }
+
+        return diffLists;
     }
 
-    private void AddPreviousItemToDiffList(List<List<long>> diffList)
+    private void AddItemToStartOfDiffList(List<List<long>> diffList)
     {
         diffList.Last().AddFirst(0);
 
         for (var i = diffList.Count - 2; i >= 0; i--)
         {
             diffList[i].AddFirst(diffList[i].First() - diffList[i + 1].First());
+        }
+    }
+
+    private void AddItemToEndOfDiffList(List<List<long>> diffList)
+    {
+        diffList.Last().Add(0);
+
+        for (var i = diffList.Count - 2; i >= 0; i--)
+        {
+            diffList[i].Add(diffList[i].Last() + diffList[i + 1].Last());
         }
     }
 }
