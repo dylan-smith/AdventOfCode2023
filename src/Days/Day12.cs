a3df592170ef85704a5d31b41db61624d46ee755
Dylan Smith <dylanfromwinnipeg@gmail.com>
2024-08-14 16:45:06 -0500

    Day 12 refactor

--- src/Days/Day12.cs
+++ src/Days/Day12.cs
@@ -1,35 +1,22 @@
-﻿
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks.Sources;
-using System.Windows.Navigation;
-
-namespace AdventOfCode.Days;
+﻿namespace AdventOfCode.Days;
 
 [Day(2023, 12)]
 public class Day12 : BaseDay
 {
-    private SpringStatus[] _sharedMemory;
-    private List<int> _groups;
-    private List<SpringStatus> _springs;
-    private long _validCount = 0L;
     private Dictionary<(int position, int group, int damagedCount), long> _seen = new Dictionary<(int position, int group, int damagedCount), long>();
 
     public override string PartOne(string input)
     {
-        var rows = input.ParseLines(ParseLine).ToList();
+        var rows = input.ParseLines(ParseLine);
+        var result = 0L;
 
         foreach (var (Springs, Groups) in rows)
         {
-            _groups = Groups;
-            _springs = Springs;
-            _sharedMemory = new SpringStatus[Springs.Count];
-
-            CountSolutions(Springs.Count, Groups.Count);
+            _seen = new Dictionary<(int position, int group, int damagedCount), long>();
+            result += CountSolutions(position: 0, group: 0, damagedCount: 0, Springs, Groups);
         }
 
-        return _validCount.ToString();
+        return result.ToString();
     }
 
     private (List<SpringStatus> Springs, List<int> Groups) ParseLine(string line)
@@ -43,24 +30,20 @@ public class Day12 : BaseDay
 
     public override string PartTwo(string input)
     {
-        var rows = input.ParseLines(ParseLine).ToList();
-        rows = rows.Select(r => ExpandRow(r)).ToList();
+        var rows = input.ParseLines(ParseLine);
+        rows = rows.Select(r => ExpandRow(r));
         var result = 0L;
 
         foreach (var (Springs, Groups) in rows)
         {
-            _groups = Groups;
-            _springs = Springs;
-            _sharedMemory = Springs.ToArray();
-
             _seen = new Dictionary<(int position, int group, int damagedCount), long>();
-            result += CountSolutions2(position: 0, group: 0, damagedCount: 0);
+            result += CountSolutions(position: 0, group: 0, damagedCount: 0, Springs, Groups);
         }
 
         return result.ToString();
     }
 
-    private long CountSolutions2(int position, int group, int damagedCount)
+    private long CountSolutions(int position, int group, int damagedCount, List<SpringStatus> springs, List<int> groups)
     {
         var result = 0L;
 
@@ -69,27 +52,27 @@ public class Day12 : BaseDay
             return value;
         }
 
-        if (damagedCount > 0 && group == _groups.Count)
+        if (damagedCount > 0 && group == groups.Count)
         {
             _seen.Add((position, group, damagedCount), 0);
             return 0;
         }
 
-        if (damagedCount > 0 && damagedCount > _groups[group])
+        if (damagedCount > 0 && damagedCount > groups[group])
         {
             _seen.Add((position, group, damagedCount), 0);
             return 0;
         }
 
-        if (position >= _sharedMemory.Length)
+        if (position >= springs.Count)
         {
-            if (damagedCount > 0 && damagedCount == _groups[group] && group == (_groups.Count - 1))
+            if (damagedCount > 0 && damagedCount == groups[group] && group == (groups.Count - 1))
             {
                 _seen.Add((position, group, damagedCount), 1);
                 return 1;
             }
 
-            if (damagedCount == 0 && group == _groups.Count)
+            if (damagedCount == 0 && group == groups.Count)
             {
                 _seen.Add((position, group, damagedCount), 1);
                 return 1;
@@ -99,153 +82,81 @@ public class Day12 : BaseDay
             return 0;
         }
 
-        var current = _sharedMemory[position];
+        var current = springs[position];
 
         if (current != SpringStatus.Unknown)
         {
             if (current == SpringStatus.Damaged)
             {
-                result = CountSolutions2(position + 1, group, damagedCount + 1);
+                result = CountSolutions(position + 1, group, damagedCount + 1, springs, groups);
                 _seen.Add((position, group, damagedCount), result);
                 return result;
             }
 
-            if (damagedCount > 0 && current == SpringStatus.Operational && damagedCount < _groups[group])
+            if (damagedCount > 0 && current == SpringStatus.Operational && damagedCount < groups[group])
             {
                 _seen.Add((position, group, damagedCount), 0);
                 return 0;
             }
 
-            if (damagedCount > 0 && damagedCount == _groups[group])
+            if (damagedCount > 0 && damagedCount == groups[group])
             {
-                result = CountSolutions2(position + 1, group + 1, 0);
+                result = CountSolutions(position + 1, group + 1, 0, springs, groups);
                 _seen.Add((position, group, damagedCount), result);
                 return result;
             }
 
-            result =  CountSolutions2(position + 1, group, damagedCount);
+            result =  CountSolutions(position + 1, group, damagedCount, springs, groups);
             _seen.Add((position, group, damagedCount), result);
             return result;
         }
 
-        if (damagedCount > 0 && damagedCount < _groups[group])
+        if (damagedCount > 0 && damagedCount < groups[group])
         {
-            result =  CountSolutions2(position + 1, group, damagedCount + 1);
+            result =  CountSolutions(position + 1, group, damagedCount + 1, springs, groups);
             _seen.Add((position, group, damagedCount), result);
             return result;
         }
 
-        if (damagedCount > 0 && damagedCount == _groups[group])
+        if (damagedCount > 0 && damagedCount == groups[group])
         {
-            result = CountSolutions2(position + 1, group + 1, 0);
+            result = CountSolutions(position + 1, group + 1, 0, springs, groups);
             _seen.Add((position, group, damagedCount), result);
             return result;
         }
 
-        if (group == _groups.Count)
+        if (group == groups.Count)
         {
-            result = CountSolutions2(position + 1, group, 0);
+            result = CountSolutions(position + 1, group, 0, springs, groups);
             _seen.Add((position, group, damagedCount), result);
             return result;
         }
 
-        result += CountSolutions2(position + 1, group, 0);
+        result += CountSolutions(position + 1, group, 0, springs, groups);
 
-        if (group < _groups.Count)
+        if (group < groups.Count)
         {
-            result += CountSolutions2(position + 1, group, 1);
+            result += CountSolutions(position + 1, group, 1, springs, groups);
         }
 
         _seen.Add((position, group, damagedCount), result);
         return result;
     }
 
-    private void IsValid()
-    {
-        for (var i = 0; i < _springs.Count; i++)
-        {
-            if (_springs[i] != SpringStatus.Unknown && _sharedMemory[i] != _springs[i])
-            {
-                return;
-            }
-        }
-
-        _validCount++;
-    }
-
-    private void CountSolutions(int springsCount, int groupsCount)
-    {
-        var springsSkip = _sharedMemory.Length - springsCount;
-
-        if (groupsCount == 0)
-        {
-            for (var i = 0; i < springsCount; i++)
-            {
-                _sharedMemory[springsSkip + i] = SpringStatus.Operational;
-            }
-
-            IsValid();
-        }
-        else
-        {
-            var groupsSkip = _groups.Count - groupsCount;
-            var currentGroup = _groups[groupsSkip];
-            var groupSum = 0;
-
-            for (var i = groupsSkip; i < _groups.Count; i++)
-            {
-                groupSum += _groups[i];
-            }
-
-            var maxOffset = springsCount - (groupSum + groupsCount - 1);
-
-            for (var offset = 0; offset <= maxOffset; offset++)
-            {
-                var pos = 0;
-
-                for (var i = 0; i < offset; i++)
-                {
-                    _sharedMemory[springsSkip + pos++] = SpringStatus.Operational;
-                }
-
-                for (var i = 0; i < currentGroup; i++)
-                {
-                    _sharedMemory[springsSkip + pos++] = SpringStatus.Damaged;
-                }
-
-                if (groupsCount > 1)
-                {
-                    _sharedMemory[springsSkip + pos++] = SpringStatus.Operational;
-                }
-
-                var newLength = springsCount - pos;
-
-                CountSolutions(newLength, groupsCount - 1);
-            }
-        }
-    }
-
     private (List<SpringStatus> Springs, List<int> Groups) ExpandRow((List<SpringStatus> Springs, List<int> Groups) row)
     {
         var springs = new List<SpringStatus>();
-
-        springs.AddRange(row.Springs);
-        springs.Add(SpringStatus.Unknown);
-        springs.AddRange(row.Springs);
-        springs.Add(SpringStatus.Unknown);
-        springs.AddRange(row.Springs);
-        springs.Add(SpringStatus.Unknown);
-        springs.AddRange(row.Springs);
-        springs.Add(SpringStatus.Unknown);
-        springs.AddRange(row.Springs);
-
         var groups = new List<int>();
 
-        groups.AddRange(row.Groups);
-        groups.AddRange(row.Groups);
-        groups.AddRange(row.Groups);
-        groups.AddRange(row.Groups);
-        groups.AddRange(row.Groups);
+        Enumerable.Range(0, 5).ForEach(_ =>
+        {
+            springs.AddRange(row.Springs);
+            springs.Add(SpringStatus.Unknown);
+        });
+
+        springs.RemoveLast();
+
+        Enumerable.Range(0, 5).ForEach(_ => groups.AddRange(row.Groups));
 
         return (springs, groups);
     }
