8c6a9a0ed7d12cf1a79451254a97372e8e9c9b6d
Dylan Smith <dylanfromwinnipeg@gmail.com>
2024-08-16 15:22:46 -0500

    day 13 refactored

--- src/Days/Day13.cs
+++ src/Days/Day13.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace AdventOfCode.Days;
+﻿namespace AdventOfCode.Days;
 
 [Day(2023, 13)]
 public class Day13 : BaseDay
@@ -16,7 +14,9 @@ public class Day13 : BaseDay
 
             for (var x = 0; x < grid.Width() - 1; x++)
             {
-                if (CanReflectVertical(grid, x))
+                var smudgeCount = CanReflectVertical(grid, x);
+
+                if (smudgeCount == 0)
                 {
                     result += x + 1;
                 }
@@ -24,7 +24,9 @@ public class Day13 : BaseDay
 
             for (var y = 0; y < grid.Height() - 1; y++)
             {
-                if (CanReflectHorizontal(grid, y))
+                var smudgeCount = CanReflectHorizontal(grid, y);
+
+                if (smudgeCount == 0)
                 {
                     result += 100 * (y + 1);
                 }
@@ -33,94 +35,6 @@ public class Day13 : BaseDay
         }
 
         return result.ToString();
-    }
-
-    private bool CanReflectVertical(char[,] grid, int mirror)
-    {
-        for (var x = 0; x <= mirror; x++)
-        {
-            var mirrorX = mirror + (mirror - x) + 1;
-
-            if (mirrorX < grid.Width())
-            {
-                for (var y = 0; y < grid.Height(); y++)
-                {
-                    if (grid[x, y] != grid[mirrorX, y])
-                    {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private bool CanReflectHorizontal(char[,] grid, int mirror)
-    {
-        for (var y = 0; y <= mirror; y++)
-        {
-            var mirrorY = mirror + (mirror - y) + 1;
-
-            if (mirrorY < grid.Height())
-            {
-                for (var x = 0; x < grid.Width(); x++)
-                {
-                    if (grid[x, y] != grid[x, mirrorY])
-                    {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private int CanReflectVertical2(char[,] grid, int mirror)
-    {
-        var result = 0;
-
-        for (var x = 0; x <= mirror; x++)
-        {
-            var mirrorX = mirror + (mirror - x) + 1;
-
-            if (mirrorX < grid.Width())
-            {
-                for (var y = 0; y < grid.Height(); y++)
-                {
-                    if (grid[x, y] != grid[mirrorX, y])
-                    {
-                        result++;
-                    }
-                }
-            }
-        }
-
-        return result;
-    }
-
-    private int CanReflectHorizontal2(char[,] grid, int mirror)
-    {
-        var result = 0;
-
-        for (var y = 0; y <= mirror; y++)
-        {
-            var mirrorY = mirror + (mirror - y) + 1;
-
-            if (mirrorY < grid.Height())
-            {
-                for (var x = 0; x < grid.Width(); x++)
-                {
-                    if (grid[x, y] != grid[x, mirrorY])
-                    {
-                        result++;
-                    }
-                }
-            }
-        }
-
-        return result;
     }
 
     public override string PartTwo(string input)
@@ -134,8 +48,8 @@ public class Day13 : BaseDay
 
             for (var x = 0; x < grid.Width() - 1; x++)
             {
-                var smudgeCount = CanReflectVertical2(grid, x);
-                
+                var smudgeCount = CanReflectVertical(grid, x);
+
                 if (smudgeCount == 1)
                 {
                     result += x + 1;
@@ -144,7 +58,7 @@ public class Day13 : BaseDay
 
             for (var y = 0; y < grid.Height() - 1; y++)
             {
-                var smudgeCount = CanReflectHorizontal2(grid, y);
+                var smudgeCount = CanReflectHorizontal(grid, y);
 
                 if (smudgeCount == 1)
                 {
@@ -156,4 +70,52 @@ public class Day13 : BaseDay
 
         return result.ToString();
     }
+
+    private int CanReflectVertical(char[,] grid, int mirror)
+    {
+        var result = 0;
+
+        for (var x = 0; x <= mirror; x++)
+        {
+            var mirrorX = mirror + (mirror - x) + 1;
+
+            if (mirrorX < grid.Width())
+            {
+                for (var y = 0; y < grid.Height(); y++)
+                {
+                    if (grid[x, y] != grid[mirrorX, y])
+                    {
+                        result++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int CanReflectHorizontal(char[,] grid, int mirror)
+    {
+        var result = 0;
+
+        for (var y = 0; y <= mirror; y++)
+        {
+            var mirrorY = mirror + (mirror - y) + 1;
+
+            if (mirrorY < grid.Height())
+            {
+                for (var x = 0; x < grid.Width(); x++)
+                {
+                    if (grid[x, y] != grid[x, mirrorY])
+                    {
+                        result++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    
 }
