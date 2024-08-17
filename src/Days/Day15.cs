66054fa1b722cd25b379f714a6f7fb1a1e1a1dee
Dylan Smith <dylanfromwinnipeg@gmail.com>
2024-08-16 23:26:18 -0500

    Day 15.1

--- src/Days/Day15.cs
+++ src/Days/Day15.cs
@@ -1,11 +1,34 @@
-﻿namespace AdventOfCode.Days;
+﻿
+namespace AdventOfCode.Days;
 
 [Day(2023, 15)]
 public class Day15 : BaseDay
 {
     public override string PartOne(string input)
     {
-        return string.Empty;
+        var commands = input.Words();
+        var result = 0L;
+
+        foreach (var command in commands)
+        {
+            result += CalcHash(command);
+        }
+
+        return result.ToString();
+    }
+
+    private long CalcHash(string command)
+    {
+        var hash = 0;
+
+        foreach (var c in command)
+        {
+            hash += c;
+            hash *= 17;
+            hash %= 256;
+        }
+
+        return hash;
     }
 
     public override string PartTwo(string input)
